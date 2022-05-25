38dbd69d187e0c427dafb8b02585b17681a8dd60
bohdan <bodyan1108@gmail.com>
2022-05-25 09:55:03 +0300

    fixed solution

--- app/main.py
+++ app/main.py
@@ -21,8 +21,6 @@ class Distance:
         return self
 
     def __mul__(self, other):
-        if isinstance(other, Distance):
-            return Distance(self.km * other.km)
         return Distance(self.km * other)
 
     def __truediv__(self, other):
@@ -32,22 +30,20 @@ class Distance:
         return self.km
 
     def __eq__(self, other):
-        if isinstance(other, Distance):
-            return self.km == other.km
         return self.km == other
 
     def __gt__(self, other):
-        if isinstance(other, Distance):
-            return self.km > other.km
         return self.km > other
 
     def __ge__(self, other):
         return not self < other
 
     def __lt__(self, other):
-        if isinstance(other, Distance):
-            return self.km < other.km
         return self.km < other
 
     def __le__(self, other):
         return not self > other
+
+
+distance = Distance(50)
+print(distance > 120)
